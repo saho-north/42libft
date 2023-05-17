93c77aa77b69b523888bf86fbce34622fae42af8
saho-north <plover.skimming.evening.waves@gmail.com>
2023-05-18 04:04:27 +0900

    fight

---
+++ ft_isalnum.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 03:52:25 by Saho Kitaha       #+#    #+#             */
+/*   Updated: 2023/05/18 04:01:34 by Saho Kitaha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int	ft_isalnum(int c)
+{
+	if ('0' <= c && c <= '9')
+		return (1);
+	else if ('A' <= c && c <= 'Z')
+		return (1);
+	else if ('a' <= c && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+// int	main(void)
+// {
+// 	int	c;
+
+// 	c = '0';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = '9';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = 'A';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = 'Z';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = 'a';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = 'z';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = '@';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = '$';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = '\n';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	c = '\t';
+// 	printf("ft_isalnum = %d\n", ft_isalnum(c));
+// 	printf("isalnum    = %d\n", isalnum(c));
+// 	return (0);
+// }
