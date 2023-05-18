4d1c3ecde18ef56abb10eda01987c2f8597ae19f
Saho Kitahara <sakitaha@c1r25s6.42tokyo.jp>
2023-05-18 19:22:05 +0900

    modified

---
+++ ft_strchr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 19:01:18 by Saho Kitaha       #+#    #+#             */
+/*   Updated: 2023/05/18 19:21:51 by Saho Kitaha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strchr(char *s, int c)
+{
+	char	to_find;
+
+	to_find = (char)c;
+	if (to_find == '\0')
+	{
+		while (*s)
+			s++;
+		return (s);
+	}
+	while (*s)
+	{
+		if (*s == to_find)
+			return (s);
+		s++;
+	}
+	return (0);
+}
+
+char	*str = "ABCDEFG";
+
+int	main(void)
+{
+	printf("ft_strchr for C : %s\n", ft_strchr(str, 'C'));
+	printf("strchr    for C : %s\n", strchr(str, 'C'));
+}
