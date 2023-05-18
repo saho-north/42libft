36b8379584f7869fbca4c641e8a58b8684bc4e9b
Saho Kitahara <sakitaha@c1r25s6.42tokyo.jp>
2023-05-18 17:22:28 +0900

    modified

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 12:57:37 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/18 13:10:01 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:22:17 by Saho Kitaha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	return (dst);
+	void	*ptr;
+
+	ptr = dst;
+	while (n > 0)
+	{
+		*(char *)dst = *(char *)src;
+		dst++;
+		src++;
+		n--;
+	}
+	return (ptr);
 }
 
-char	src1[] = "ABCDEFG";
-char	dst1[] = "aaaaaaaaaaa";
-char	dst2[] = "aaaaaaaaaaa";
+// char	src[] = "ABCDEFG";
+// char	dst1[] = "aaaaaaaaaaa";
+// char	dst2[] = "aaaaaaaaaaa";
 
-int	main(void)
-{
-	printf("ft_memcpy = %s\n", (char *)ft_memcpy(dst1, src1, 3));
-	printf("memcpy    = %s\n", (char *)memcpy(dst2, src1, 3));
-}
+// int	main(void)
+// {
+// 	printf("ft_memcpy = %s\n", (char *)ft_memcpy(dst1, src, 3));
+// 	printf("memcpy    = %s\n", (char *)memcpy(dst2, src, 3));
+// }
