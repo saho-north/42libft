85a295b42ebd465df89dfbde02477ff8d777d96e
saho-north <plover.skimming.evening.waves@gmail.com>
2023-05-19 03:20:36 +0900

    modified

---
+++ ft_strjoin.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 02:39:12 by sakitaha          #+#    #+#             */
+/*   Updated: 2023/05/19 03:20:26 by sakitaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	cat_str(char const *s1, char const *s2, char *ptr)
+{
+	while (*s1)
+	{
+		*(ptr++) = *(s1++);
+	}
+	while (*s2)
+	{
+		*(ptr++) = *(s2++);
+	}
+	*ptr = '\0';
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (len == 0)
+	{
+		ptr = (char *)malloc(sizeof(char) * 1);
+		*ptr = 0;
+		return (ptr);
+	}
+	ptr = (char *)malloc(len + 1);
+	if (!ptr)
+		return (0);
+	cat_str(s1, s2, ptr);
+	return (ptr);
+}
+
+// char	*sep = "...";
+// char	*sep5 = "123";
+// char	*str1 = "";
+// char	*str2 = "Hello";
+// char	*str3 = "Hello,  world ";
+// char	*str4 = "Hello, world! this is a test";
+// char	*str5 = "apple banana grape orange";
+
+// int	main(void)
+// {
+// 	char	*result;
+
+// 	// Test case 1: No strings
+// 	result = ft_strjoin(str1, sep);
+// 	printf("Test 1: %s\n", result);
+// 	free(result);
+// 	// Test case 2: One string
+// 	result = ft_strjoin(str2, sep);
+// 	printf("Test 2: %s\n", result);
+// 	free(result);
+// 	// Test case 3: Two strings
+// 	result = ft_strjoin(str3, sep);
+// 	printf("Test 3: %s\n", result);
+// 	free(result);
+// 	// Test case 4: Multiple strings with non-alphanumeric separator
+// 	result = ft_strjoin(str4, sep);
+// 	printf("Test 4: %s\n", result);
+// 	free(result);
+// 	// Test case 5: Multiple strings with alphanumeric separator
+// 	result = ft_strjoin(str5, sep5);
+// 	printf("Test 5: %s\n", result);
+// 	free(result);
+// 	return (0);
+// }
---
+++ ft_substr.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 02:15:54 by sakitaha          #+#    #+#             */
+/*   Updated: 2023/05/19 02:37:44 by sakitaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+s:  The string from which to create the substring.
+
+start:  The start index of the substring in the string ’s’.
+
+len:  The maximum length of the substring.
+
+Allocates (with malloc(3)) and returns a substring from the string ’s’.
+
+The substring begins at index ’start’ and is of maximum size ’len’.
+*/
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substring;
+	size_t	i;
+
+	substring = 0;
+	substring = (char *)malloc(len + 1);
+	while (start)
+	{
+		s++;
+		start--;
+	}
+	i = 0;
+	while (i < len && s[i])
+	{
+		substring[i] = s[i];
+		i++;
+	}
+	substring[i] = '\0';
+	return (substring);
+}
+
+int	main(void)
+{
+	const char		*str;
+	unsigned int	start;
+	size_t			len;
+	char			*sub;
+
+	str = "Hello, world!";
+	start = 7;
+	len = 5;
+	sub = ft_substr(str, start, len);
+	if (sub)
+	{
+		printf("%s\n", sub); // "world" を表示
+		free(sub);
+	}
+	return (0);
+}
