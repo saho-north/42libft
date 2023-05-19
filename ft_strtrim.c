8fbf90de1e6e62d7a47ff80f3a6f13ef797e8ddb
saho-north <plover.skimming.evening.waves@gmail.com>
2023-05-19 17:14:22 +0900

    modified

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,14 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/19 12:07:37 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/19 12:39:00 by sakitaha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
 ft_strtrim
@@ -24,53 +18,111 @@ Allocates (with malloc(3)) and returns a copy of
 from the beginning and the end of the string.
 */
 
-int	ft_trimmedstrlen(char const *s1, char const *set)
+int	count_strings(char *str, char *charset)
 {
-	int		len;
-	int		head;
-	int		to_trim;
-	char	*result;
+	int		head_count;
+	bool	in_words_flag;
 
-	len = 0;
-	head = 0;
-	while (s1[head])
+	head_count = 0;
+	in_words_flag = false;
+	while (*str != '\0')
 	{
-		to_trim = 0;
-		if (s1[head] != set[0])
+		if (!in_words_flag && !(is_charset(*str, charset)))
 		{
-			len++;
-			head++;
+			in_words_flag = true;
+			head_count++;
 		}
-		else
-		{
-			while (s1[head + to_trim] == set[to_trim] && s1[head + to_trim])
-				to_trim++;
-			if (!set[to_trim])
-				return (0);
-		}
+		else if (in_words_flag && is_charset(*str, charset))
+			in_words_flag = false;
+		str++;
 	}
+	return (head_count);
+}
+
+char	**splitter(char *str, char *charset, char **dest, int head_count)
+{
+	int	str_no;
+	int	str_len;
+	int	index;
+
+	str_no = 0;
+	str_len = 0;
+	while (*str && str_no < head_count)
+	{
+		if (!is_charset(*str, charset))
+		{
+			str_len = not_charset_strlen(str, charset);
+			dest[str_no] = (char *)malloc(sizeof(char) * (str_len + 1));
+			if (!dest[str_no])
+				return (0);
+			index = 0;
+			while (*str && !is_charset(*str, charset))
+			{
+				dest[str_no][index++] = *str;
+				str++;
+			}
+			dest[str_no++][index] = '\0';
+		}
+		str++;
+	}
+	return (dest);
+}
+
+bool	is_charset(char c, char *charset)
+{
+	while (*charset != '\0')
+	{
+		if (c == *charset)
+			return (true);
+		charset++;
+	}
+	return (false);
+}
+
+void	trimmer(char *str, char *charset, char *dest)
+{
+	while (*str)
+	{
+		if
+	}
+}
+
+int	trimmed_len(char const *s1, char const *set)
+{
+	int	dest_len;
+
+	dest_len = 0;
+	while (*s1)
+	{
+		if (!is_charset(*s1, set))
+		{
+			dest_len++;
+		}
+		s1++;
+	}
+	return (dest_len);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char	*dest;
+	int		dest_len;
 	int		head;
 	int		to_trim;
-	char	*result;
 
 	if (!s1 || !set)
-		return (s1);
-	head = 0;
-	while (s1[head])
-	{
-		to_trim = 0;
-		while (s1[head + to_trim] == set[to_trim] && s1[head + to_trim])
-		{
-		}
-	}
+		return (0);
+	dest_len = trimmed_len(s1, set);
+	dest = 0;
+	dest = (char *)malloc(dest_len + 1);
+	if (!dest)
+		return (0);
+	dest[dest_len] = 0;
+	return (dest);
 }
 
-char	*str = "My 42 friend loves his 42 cats.";
-char	*charset = "42 ";
+char	*str = "My 42_friend loves his 42_cats.";
+char	*charset = "42_";
 
 int	main(void)
 {
