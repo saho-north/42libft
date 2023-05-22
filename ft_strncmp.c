04793b31e2b5e4559ca6c8be9b6fe979ed9e3c27
Saho Kitahara <sakitaha@c1r25s6.42tokyo.jp>
2023-05-22 21:40:59 +0900

    OK

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 20:55:10 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/20 23:46:36 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/05/22 20:12:13 by Saho Kitaha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	while (i < n && (s1[i] != 0 || s2[i] != 0))
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n && (str1[i] != 0 || str2[i] != 0))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return ((str1[i] - str2[i]));
 		i++;
 	}
 	return (0);
@@ -33,6 +37,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 // const char	*str4 = "AB";
 // const char	*str5 = "B";
 // const char	*str6 = "A";
+// const char	*s1 = "\200";
+// const char	*s2 = "\0";
 
 // int	main(void)
 // {
@@ -49,6 +55,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 // 	printf("strncmp    : ABC: B     = %d\n", strncmp(str0, str5, 2));
 // 	printf("ft_strncmp : ABC: B     = %d\n\n", ft_strncmp(str0, str5, 2));
 // 	printf("strncmp    : ABC: A     = %d\n", strncmp(str0, str6, 2));
-// 	printf("ft_strncmp : ABC: A     = %d\n", ft_strncmp(str0, str6, 2));
+// 	printf("ft_strncmp : ABC: A     = %d\n\n", ft_strncmp(str0, str6, 2));
+// 	printf("strncmp    : ABC: A     = %d\n", strncmp(s1, s2, 1));
+// 	printf("ft_strncmp : ABC: A     = %d\n", ft_strncmp(s1, s2, 1));
 // 	return (0);
 // }
