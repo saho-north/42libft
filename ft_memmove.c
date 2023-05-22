71e46bf6c22f6ef2f4e2a3933eefb1cbbe90f9fb
Saho Kitahara <sakitaha@c1r25s6.42tokyo.jp>
2023-05-22 15:39:09 +0900

    modified

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 17:23:42 by Saho Kitaha       #+#    #+#             */
-/*   Updated: 2023/05/20 12:57:58 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/05/22 14:25:57 by Saho Kitaha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,32 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*ptrdest;
 	char	*ptrsrc;
+	size_t	index;
 
 	ptrdest = (char *)dst;
 	ptrsrc = (char *)src;
-	if (ptrdest == NULL || ptrsrc == NULL)
+	if (ptrdest == NULL && ptrsrc == NULL)
 	{
 		return (NULL);
 	}
-	while (len > 0)
+	if (dst > src)
 	{
-		if (&ptrdest[len - 1] != NULL && &ptrsrc[len - 1] != NULL)
-			ptrdest[len - 1] = ptrsrc[len - 1];
-		len--;
+		while (len > 0)
+		{
+			if (&ptrdest[len - 1] != NULL && &ptrsrc[len - 1] != NULL)
+				ptrdest[len - 1] = ptrsrc[len - 1];
+			len--;
+		}
+	}
+	else
+	{
+		index = 0;
+		while (index < len)
+		{
+			if (&ptrdest[index] != NULL && &ptrsrc[index] != NULL)
+				ptrdest[index] = ptrsrc[index];
+			index++;
+		}
 	}
 	return (dst);
 }
@@ -38,11 +52,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 // int	main(void)
 // {
-// 	// テストケース1: オーバーラップがない場合
 // 	printf("ft_memmove = %s\n", (char *)ft_memmove(str1, &str1[3], 3));
-// 	printf("memmove    = %s\n\n", (char *)memmove(str2, &str2[3], 3));
-// 	// テストケース2: オーバーラップがある場合
+// 	printf("memmove    = %s\n", (char *)memmove(str2, &str2[3], 3));
 // 	printf("ft_memmove = %s\n", (char *)ft_memmove(&str3[3], str3, 3));
 // 	printf("memmove    = %s\n", (char *)memmove(&str3[3], str3, 3));
+// 	printf("ft_ = %s\n", (char *)ft_memmove(((void *)0), ((void *)0), 5));
+// 	printf("lib = %s\n", (char *)memmove(((void *)0), ((void *)0), 5));
 // 	return (0);
 // }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,78 +1,68 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/21 00:51:21 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/22 13:40:07 by sakitaha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "libft.h"
 
-/*
-(malloc(3)で)確保し、文字列's'の各文字に関数 'f' を適用した新しい文字列を返す。
+// #include "libft.h"
 
-s:  The string on which to iterate.
-f:  The function to apply to each character.
+// /*
+// (malloc(3)で)確保し、文字列's'の各文字に関数 'f' を適用した新しい文字列を返す。
 
-Return value
-The string created from the successive applications of ’f’.
-Returns NULL if the allocation fails.
+// s:  The string on which to iterate.
+// f:  The function to apply to each character.
 
-Description
-Applies the function ’f’ to each character of the
-string ’s’, and passing its index as first argument
-to create a new string (with malloc(3)) resulting
-from successive applications of ’f’.
- */
+// Return value
+// The string created from the successive applications of ’f’.
+// Returns NULL if the allocation fails.
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-}
+// Description
+// Applies the function ’f’ to each character of the
+// string ’s’, and passing its index as first argument
+// to create a new string (with malloc(3)) resulting
+// from successive applications of ’f’.
+//  */
 
-int	main(int argc, char const *argv[])
-{
-	char	*test1;
+// char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+// {
+// }
 
-	(void)argc;
-	(void)argv;
-	test1 = "abcde";
-	printf("ft_strmapi(%s) = %s\n", test1, ft_strmapi("abcde", &ft_toupper));
-	return (0);
-}
+// int	main(int argc, char const *argv[])
+// {
+// 	char	*test1;
 
-#include <stdio.h>
+// 	(void)argc;
+// 	(void)argv;
+// 	test1 = "abcde";
+// 	printf("ft_strmapi(%s) = %s\n", test1, ft_strmapi("abcde", &ft_toupper));
+// 	return (0);
+// }
 
-char	ft_toupper(unsigned int index, char c)
-{
-	// テスト用に文字を大文字に変換する関数
-	if (c >= 'a' && c <= 'z')
-		return (c - 32); // 大文字に変換
-	else
-		return (c); // 変換しない
-}
+// #include <stdio.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	// ここに関数の実装を追加する
-}
+// char	ft_toupper(unsigned int index, char c)
+// {
+// 	// テスト用に文字を大文字に変換する関数
+// 	if (c >= 'a' && c <= 'z')
+// 		return (c - 32); // 大文字に変換
+// 	else
+// 		return (c); // 変換しない
+// }
 
-int	main(void)
-{
-	char	*test1;
-	char	*expected_output1;
-	char	*output1;
+// char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+// {
+// 	// ここに関数の実装を追加する
+// }
 
-	test1 = "abcde";
-	expected_output1 = "ABCDE";
-	output1 = ft_strmapi(test1, &ft_toupper);
-	printf("Input: %s\n", test1);
-	printf("Expected Output: %s\n", expected_output1);
-	printf("Output: %s\n", output1);
-	printf("\n");
-	// 他のテストケースも追加可能
-	return (0);
-}
+// int	main(void)
+// {
+// 	char	*test1;
+// 	char	*expected_output1;
+// 	char	*output1;
+
+// 	test1 = "abcde";
+// 	expected_output1 = "ABCDE";
+// 	output1 = ft_strmapi(test1, &ft_toupper);
+// 	printf("Input: %s\n", test1);
+// 	printf("Expected Output: %s\n", expected_output1);
+// 	printf("Output: %s\n", output1);
+// 	printf("\n");
+// 	// 他のテストケースも追加可能
+// 	return (0);
+// }
