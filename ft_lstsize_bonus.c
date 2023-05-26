5cdbaf93bd7c217d457dab29b45ca51f04ccae5b
saho-north <plover.skimming.evening.waves@gmail.com>
2023-05-26 19:07:52 +0900

    modified

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,17 +6,11 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:30:16 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/26 19:06:05 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:07:19 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-lst:  The beginning of the list.
-The length of the list
-Counts the number of nodes in a list.
- */
 
 int	ft_lstsize(t_list *lst)
 {
