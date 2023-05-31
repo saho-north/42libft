6ff586f7dac10e2aae2aea52aa2933dd82f4641a
Saho Kitahara <sakitaha@c1r25s6.42tokyo.jp>
2023-05-31 17:30:39 +0900

    modified

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -3,51 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: Saho Kitahara <sakitaha@student.42tokyo    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 14:42:40 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/05/31 15:58:03 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:30:14 by Saho Kitaha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+static t_list	*lstmaper(t_list *head, t_list *lst, void *(*f)(void *),
+		void (*del)(void *))
 {
-	t_list	*head;
 	t_list	*prev;
 	t_list	*new;
-	void	*content;
-	bool	is_first;
 
-	head = NULL;
 	prev = NULL;
 	new = NULL;
-	content = NULL;
-	is_first = true;
-	if (!lst || !f || !del)
-		return (NULL);
+	prev = head;
+	lst = lst->next;
 	while (lst)
 	{
-		content = f(lst->content);
-		new = ft_lstnew(content);
+		new = ft_lstnew(f(lst->content));
 		if (!new)
 		{
 			ft_lstclear(&head, del);
 			return (NULL);
 		}
-		if (is_first)
-		{
-			head = new;
-			is_first = false;
-			prev = head;
-		}
-		else
-		{
-			prev->next = new;
-			prev = prev->next;
-		}
+		prev->next = new;
+		prev = prev->next;
 		lst = lst->next;
 	}
+	return (head);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*head;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	head = NULL;
+	head = ft_lstnew(f(lst->content));
+	if (!head)
+		return (NULL);
+	head = lstmaper(head, lst, f, del);
 	return (head);
 }
