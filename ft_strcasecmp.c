bd16f0402a3bab4ba61a27f9071306179887f2a7
saho-north <SahoKitahara@my.uopeople.edu>
2023-11-27 21:29:26 +0900

    ok

--- ft_strcasecmp.c
+++ ft_strcasecmp.c
@@ -6,11 +6,17 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:03:24 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/11/27 18:34:17 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/11/27 21:28:47 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+ * The strcasecmp() function compares the two strings s1 and s2,
+ * ignoring the case of the characters.
+ * It is the caller's responsibility to ensure non-NULL arguments.
+ */
 
 int	ft_strcasecmp(const char *s1, const char *s2)
 {
