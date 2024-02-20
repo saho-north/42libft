8652b1bc9020737635561dd85016b5acfae8fe1e
saho-north <SahoKitahara@my.uopeople.edu>
2024-02-20 15:08:50 +0900

    .

--- srcs/exdlib/ft_lerpcolor.c
+++ srcs/exdlib/ft_lerpcolor.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 20:08:13 by sakitaha          #+#    #+#             */
-/*   Updated: 2024/02/20 00:59:14 by sakitaha         ###   ########.fr       */
+/*   Updated: 2024/02/20 03:02:27 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static unsigned char	lerp(unsigned char start, unsigned char end, float t)
 	return ((unsigned char)(start + (end - start) * t));
 }
 
-int	ft_lerpcolor(int color_1, int color_2, float t)
+int	ft_lerpcolor(int color_0, int color_1, float t)
 {
 	unsigned char	red;
 	unsigned char	green;
@@ -31,12 +31,12 @@ int	ft_lerpcolor(int color_1, int color_2, float t)
 	{
 		t = 1.0;
 	}
-	if (color_1 == color_2)
+	if (color_0 == color_1)
 	{
-		return (color_1);
+		return (color_0);
 	}
-	red = lerp((color_1 >> 16) & 0xFF, (color_2 >> 16) & 0xFF, t);
-	green = lerp((color_1 >> 8) & 0xFF, (color_2 >> 8) & 0xFF, t);
-	blue = lerp(color_1 & 0xFF, color_2 & 0xFF, t);
+	red = lerp((color_0 >> 16) & 0xFF, (color_1 >> 16) & 0xFF, t);
+	green = lerp((color_0 >> 8) & 0xFF, (color_1 >> 8) & 0xFF, t);
+	blue = lerp(color_0 & 0xFF, color_1 & 0xFF, t);
 	return ((red << 16) | (green << 8) | blue);
 }
